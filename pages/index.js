83b508ef156729c05705e91bc34bfed783344a5b
m7mdharoun <34725185+m7mdharoun@users.noreply.github.com>
2022-05-26 22:43:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
+res.send('<script>window.location.href="your URL";</script>');
 
       <Footer />
     </div>
